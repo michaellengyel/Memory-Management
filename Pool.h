9ffd2f65472660f010c41418b82cc0f8a79b5eb3
michaellengyel <viram24@gmail.com>
2020-06-12 01:13:41 +0200

    Adding Block linking function.

--- Pool.h
+++ Pool.h
@@ -1,15 +1,21 @@
 #pragma once
+#include <iostream>
 
 template<typename T, int N> class Pool {
 public:
 
+    // FUNCTION: Contructor O(1)
     Pool() {
         T* block = new T[N];
         begin = block;
         head = block;
         used = 0;
+
+        // Create Link
+		link(begin);
     }
 
+    // FUNCTION: Destructor O(1)
     ~Pool() {
         delete[] block;
     }
@@ -35,6 +41,31 @@ public:
     }
 
 private:
+
+    // FUNCTION: link blocks O(1) (since only runs ones in lifetime of)
+	void link(T* begin) {
+
+		T* next = begin + 1;
+		T* current = begin;
+
+		for (int i = 0; i < (N - 1); i++) {
+			T** reint_head = reinterpret_cast<T**>(current);
+			*reint_head = next;
+			next++;
+			current++;
+ 		}
+
+		T** reint_current = reinterpret_cast<T**>(current);
+		*reint_current = nullptr;
+
+		// Logging code
+		std::cout << "T Address" << "\t" << "Ptr in T" << std::endl;
+		for (int i = 0; i < (N); i++) {
+			std::cout << begin << "\t" << *begin << std::endl;
+			begin++;
+		}
+	}
+
     T* block;
     T* begin;
     T* head;
--- main.cpp
+++ main.cpp
@@ -3,18 +3,15 @@
 
 int main() {
 
-    int* test = new int[4];
-    delete[] test;
+    
+    Pool<int, 10> pool;
 
-    {
-        Pool<int, 10> pool;
-
-        int* a = pool.allocate();
-        int* b= pool.allocate(3);
-        pool.deallocate(a);
-        pool.deallocate(b, 2);
-        int* c = pool.construct(42);
-    }
+    //int* a = pool.allocate();
+    //int* b= pool.allocate(3);
+    //pool.deallocate(a);
+    //pool.deallocate(b, 2);
+    //int* c = pool.construct(42);
+    
 
     std::cout << "Testing Setup" << std::endl;
 
